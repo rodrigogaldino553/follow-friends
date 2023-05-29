24cd938174c7ee937647243461a45a2995bd35ce
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-05-29 20:57:38 -0300

    install action text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_rich_text :content
 
   scope :approved_posts, -> { where(:approved => true) }
   scope :unapproved_posts, -> { where(:approved => false) }
