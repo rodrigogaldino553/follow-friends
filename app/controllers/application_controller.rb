2d1b0b48e5bfbb2277a3d73665c66fc41f60299a
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-05-28 23:55:15 -0300

    config authorization to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  include Pundit::Authorization
+
 
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Sorry, You Are Not Authorized To Do This"
+    redirect_to(request.referrer || root_path)
   end
 end
