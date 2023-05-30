c0ebf67462ff7c038142fdc05086cd84beab66bd
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-05-29 23:53:52 -0300

    remove title from posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_234447) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_025243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_234447) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
