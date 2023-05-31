2b5afa914085165ab370db72f70198a47079ce8d
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-05-30 23:35:19 -0300

    add qrcode to user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
-  
   def user_avatar(user)
     user.avatar.attached? ? user.avatar : asset_path('default.png')
   end
 
+  def share_qrcode(user)
+    user.generate_qrcode if user.qrcode.nil?
+
+    user.qrcode
+  end
 end
