998c9a926b8792d7f43dc2daf4adb9d875e82cab
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-05-31 23:17:09 -0300

    fix unapproved posts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :posts do
-    get :unapproved_posts
+    member do
+      get :unapproved_posts
+    end
   end
+  # get '/posts/unapproved', to: 'posts#unapproved', as: 'unapproved_posts'
 
   devise_for :users, controllers: {
         registrations: 'users/registrations'
