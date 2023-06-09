bf55f6d498dd6a6f02970027d9685f0f0a1dcdf6
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-06-09 11:12:00 -0300

    fix dependent destroys bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_rich_text :content
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   scope :approved_posts, -> { where(:approved => true) }
   scope :unapproved_posts, -> { where(:approved => false) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 
   def assign_default_role
